955dbe8a448b74c096ecdf78847f73aaceb76294
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-11-21 01:38:38 +0200

    fixed create database stmt

--- github-actions-psql-init.sh
+++ github-actions-psql-init.sh
@@ -2,9 +2,8 @@ SCRIPTS_DIR=$PWD/scripts
 
 mkdir $SCRIPTS_DIR
 
-echo "
-DO
-$$
+echo "DO
+\$\$
 BEGIN
     IF NOT EXISTS (
         SELECT FROM pg_database
@@ -13,8 +12,8 @@ BEGIN
         EXECUTE 'CREATE DATABASE northwind';
     END IF;
 END
-$$;
-">$SCRIPTS_DIR/0-northwind_db.sql
+\$\$;" > $SCRIPTS_DIR/0-northwind_db.sql
+
 
 curl https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/sample/northwind_ddl.sql     -Lo    $SCRIPTS_DIR/1-northwind_ddl.sql
 curl https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/sample/northwind_data.sql    -Lo    $SCRIPTS_DIR/2-northwind_data.sql
