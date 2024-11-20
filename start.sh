43dcc9559c31a72b2939e091096272dca34573b7
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-11-20 19:59:58 +0200

    start shell script

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ SCRIPTS_DIR=$PWD/scripts
 mkdir $SCRIPTS_DIR
 
 echo "CREATE DATABASE IF NOT EXISTS northwind;"                                                  >>    $SCRIPTS_DIR/0-northwind_db.sql
+# echo "CREATE TYPE order_status AS ENUM ('pending', 'shipped');"                                  >>    $SCRIPTS_DIR/01-northwind_dwh.sql
 curl https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/sample/northwind_ddl.sql     -Lo    $SCRIPTS_DIR/1-northwind_ddl.sql
 curl https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/sample/northwind_data.sql    -Lo    $SCRIPTS_DIR/2-northwind_data.sql
-
 docker compose up -d 
 
 python -m venv dbt-env 
