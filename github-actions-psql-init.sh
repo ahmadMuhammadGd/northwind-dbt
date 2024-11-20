7896f198661b3d81b1af704de28cf0519780f66e
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-11-21 01:29:47 +0200

    bad commit msg :(

--- github-actions-psql-init.sh
+++ github-actions-psql-init.sh
@@ -1,6 +1,3 @@
-
-
-
 SCRIPTS_DIR=$PWD/scripts
 
 mkdir $SCRIPTS_DIR
@@ -18,12 +15,7 @@ BEGIN
 END
 $$;
 ">$SCRIPTS_DIR/0-northwind_db.sql
-# echo "CREATE TYPE order_status AS ENUM ('pending', 'shipped');"                                  >>    $SCRIPTS_DIR/01-northwind_dwh.sql
+
 curl https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/sample/northwind_ddl.sql     -Lo    $SCRIPTS_DIR/1-northwind_ddl.sql
 curl https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/sample/northwind_data.sql    -Lo    $SCRIPTS_DIR/2-northwind_data.sql
 docker compose up -d 
-
-python -m venv dbt-env 
-. dbt-env/bin/activate 
-
-pip install -r requirements.txt
